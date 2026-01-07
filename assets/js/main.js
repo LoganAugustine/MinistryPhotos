bac780462064edb1de717c2e335e86431c7b1523
LoganAugustine <103656383+LoganAugustine@users.noreply.github.com>
2026-01-06 23:26:55 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,38 +236,9 @@
 
 			});
 
-		$main.poptrox({
-		  baseZIndex: 20000,
-		  caption: function($a) {
-		    var s = '';
-		    $a.nextAll().each(function() { s += this.outerHTML; });
-		    return s;
-		  },
-		  fadeSpeed: 300,
-		  onPopupClose: function() { $body.removeClass('modal-active'); },
-		  onPopupOpen: function() { $body.addClass('modal-active'); },
-		  overlayOpacity: 0,
-		  popupCloserText: '',
-		  popupHeight: 'auto',
-		  popupLoaderText: '',
-		  popupSpeed: 300,
-		  popupWidth: 'auto',
-		  selector: '.thumb > a.image',
-		  usePopupCaption: true,
-		  usePopupCloser: true,
-		  usePopupDefaultStyling: false,
-		  usePopupForceClose: true,
-		  usePopupLoader: true,
-		  usePopupNav: true,
-		  windowMargin: 50
-		});
-		
-		// Responsive window margins
-		breakpoints.on('<=xsmall', function() { $main[0]._poptrox.windowMargin = 0; });
-		breakpoints.on('>xsmall', function() { $main[0]._poptrox.windowMargin = 50; });
-
 
 })(jQuery);
+
 
 
 
